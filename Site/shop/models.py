13e7c7266b387cfcacafbc04109d503b6b9fe49d
Elnifio <elnifio0519@gmail.com>
2020-11-04 02:04:41 +0800

    Update Models and Views

--- Site/shop/models.py
+++ Site/shop/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    userid = models.AutoField(primary_key=True)
+    username = models.TextField()
+    useremail = models.EmailField()
+    userpassword = models.TextField()
+
+class Item(models.Model):
+    itemid = models.AutoField(primary_key=True)
+    stock = models.IntegerField()
+    price = models.FloatField()
+    picture = models.ImageField()
+    publisher = models.IntegerField()
+
+class PublishedItem(models.Model):
+    itemid = models.IntegerField()
+    userid = models.IntegerField()
+    date = models.DateField(auto_now = True)
+    
+class PurchasedItem(models.Model):
+    itemid = models.IntegerField()
+    userid = models.IntegerField()
+    date = models.DateField(auto_now=True)
+
+class SavedItem(models.Model):
+    itemid = models.IntegerField()
+    userid = models.IntegerField()
+    date = models.DateField(auto_now = True)
+
+
+
