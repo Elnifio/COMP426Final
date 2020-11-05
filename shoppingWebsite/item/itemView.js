f780249a6d8fa7d33ed870ebe7bf618b24dafcb3
han gao <gaohan0719@163.com>
2020-11-04 23:55:30 -0500

    added zoom function

--- shoppingWebsite/item/itemView.js
+++ shoppingWebsite/item/itemView.js
@@ -12,8 +12,8 @@ let ItemView = class{
     }
     createViewDiv(){
         let div = $(`<div class = 'itemView'>
-                        <img src='./icons/426.4.PNG' class='itemImg'/>
-                        <div class='itemText'>
+                        <img src='./icons/item1/426.4.PNG' class='itemImg'/>
+                        <div class='itemText clickZone'>
                             <p class='itemName'><strong>${this.item.name}</strong></p>
                             <div class='itemRating'></div>
                             <p class='itemPublisher'>By: ${this.item.publisher}</p>
@@ -30,7 +30,7 @@ let ItemView = class{
                 $(ratingDiv).append('&#9734;');
             }
         }
-        div.find('.itemImg').on('click', (e) => {
+        div.find('.clickZone').on('click', (e) => {
             let newDiv = this.createDetailedView();
             this.curDiv.replaceWith(newDiv);
             this.curDiv = newDiv;
@@ -40,19 +40,23 @@ let ItemView = class{
 
     createDetailedView(){
         let div = $(`<div class = 'itemDetailView'>
-                        <img src='./icons/426.4.PNG' class='itemDetailImg'/>
+                        <div class='img-zoom'>
+                            <img src='./icons/item1/426.4.PNG' class='itemDetailImg'/>
+                        </div>
                         <div class='itemDetailText'>
+                            <div class= 'clickZone'>
                             <p class='itemDetailName'><strong>${this.item.name}</strong></p>
                                 <div class='itemDetailRating'></div>
                             <p class='itemDetailPublisher'>By: ${this.item.publisher}</p>
                             <p class='itemDetailPrice'>&#36;${this.item.price}</p>
                             <p class='itemDetailDescription'>${this.item.description}</p>
+                            </div>
                             <br>
                             <form class='purchaseForm'>
                                 <label for='amount'>Amount:</label>
                                 <select name='amount' class='amount'>
                                 </select>
-                                <button type = 'submit' class ='purchaseButton'>purchase</button>
+                                <button type = 'submit' class ='purchaseButton'>Shopping Cart</button>
                             </form>
                         </div>
                     </div>`)
@@ -75,11 +79,20 @@ let ItemView = class{
             event.preventDefault()
             this.item.purchase(purchaseDiv.find(`select[name='amount']`).val())
         })
-        div.find('.itemDetailImg').on('click', (e)=> {
+        div.find('.clickZone').on('click', (e)=> {
             let newDiv = this.createViewDiv()
             this.curDiv.replaceWith(newDiv)
             this.curDiv = newDiv
         })
+        div.find('.itemDetailImg').on('mousemove', function(event) {
+                let offset = div.find('.img-zoom').offset();
+                let x = (event.pageX- offset.left) / div.find('.itemDetailImg').width() *100
+                let y = (event.pageY- offset.top) / div.find('.itemDetailImg').height() *100
+                div.find('.itemDetailImg').css({'transform-origin': x+'% '+y+'%', 'transform': 'scale(2.5)'})
+        });
+        div.find('.itemDetailImg').on('mouseout', function(event) {
+            div.find('.itemDetailImg').css({'transform': 'scale(1)'})
+    });
         return div
     }
 
