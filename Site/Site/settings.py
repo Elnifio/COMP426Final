a51199fabe31b9a2b1bab4305d19c7100685053e
Elnifio <elnifio0519@gmail.com>
2020-11-23 13:00:41 +0800

    update settings

--- Site/Site/settings.py
+++ Site/Site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@)e%&^urhza*b3bw35pxqs&%0wm#xyk)a+@7bx@6n*kn)0ivk='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
